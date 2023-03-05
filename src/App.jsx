9de6451b2a784b08b46e5e4028630f326522d5a3
momen-ellaban <momenellaban210@gmail.com>
2023-03-05 13:11:49 +0200

    f

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import Navbar from "./components/navbar/Navbar";
+
+// import { About } from './components/about/About';
+
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+    
+
+
+    </div>
+  );
+}
+
+export default App;
